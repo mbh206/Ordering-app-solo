f7952fc4c46530a15a08e4eac476a01c97d31bdc
Mark <75957685+mbh206@users.noreply.github.com>
2022-11-01 04:48:02 +0900

    finished project

---
+++ JS/data.js
@@ -0,0 +1,76 @@
+const menuItems = [
+    {
+        name: 'Coffee',
+        description: 'Our signature roast brewed to perfection!',
+        category: drinks,
+        price: '¥350',
+        image: '#',
+        options: [{
+                size: [
+                    'Short',
+                    'Tall', 
+                    'Grande', 
+                    'Vente',
+                    'Mega'
+            ]},
+            {
+                milk: [
+                    'Half & Half',
+                    'Whole', 
+                    '2%', 
+                    'Non-Fat',
+                    'Soy',
+                    'Oat'
+            ]},
+            {
+                sugar: [
+                    'White',
+                    'Raw',
+                    'Fake'
+            ]},
+            {
+                syrup: [
+                    'Vanilla',
+                    'Hazelnut',
+                    'Irish Cream',
+                    'Strawberry'
+            ]}]
+    },
+    {
+        name: 'latte',
+        description: 'Our full bodied espresso roast ',
+        category: drinks,
+        price: '¥350',
+        image: '#',
+        options: [{
+                size: [
+                    'Short',
+                    'Tall', 
+                    'Grande', 
+                    'Vente',
+                    'Mega'
+            ]},
+            {
+                milk: [
+                    'Half & Half',
+                    'Whole', 
+                    '2%', 
+                    'Non-Fat',
+                    'Soy',
+                    'Oat'
+            ]},
+            {
+                sugar: [
+                    'White',
+                    'Raw',
+                    'Fake'
+            ]},
+            {
+                syrup: [
+                    'Vanilla',
+                    'Hazelnut',
+                    'Irish Cream',
+                    'Strawberry'
+            ]}]
+    },
+]
